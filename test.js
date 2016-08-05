0bebc86a795490290fd124a9c42a6179a8016369
Williams Medina <williams.medinaa@gmail.com>
2016-08-05 07:11:56 -0400

    update test git support

--- test.js
+++ test.js
@@ -1,28 +1,9 @@
 // Atom Bugs: Test
 var title = `Basically the only thing happening
-  here is that when the class is created,
-  it creates a simple div`
+  here is that when the`
 
 function Greet (msg) {
   console.log(msg)
 }
 
 Greet(title)
-
-// Hello
-var variables = [
-  'Hello World',
-  {
-    title: 'Some Object'
-  },
-  [1, 2, 3],
-  new Date(),
-  true,
-  123123,
-  function () {
-    console.log('hello')
-  }]
-
-variables.forEach((m) => {
-  Greet(m)
-})
