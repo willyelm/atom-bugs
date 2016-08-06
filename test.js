7a78ed9fabcf563f6fbb0e04948532f5e049c9ed
Williams Medina <williams.medinaa@gmail.com>
2016-08-05 22:40:34 -0400

    show objects in console, add next button

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 // Atom Bugs: Test
-var title = `Basically the only thing happening
-  here is that when the class is created,
-  it creates a simple div`
+var title = `This package is a integrated client of
+  NodeJS Debugger which uses the V8 Debugging Protocol,
+  in this first version it works for NodeJS applications
+  only. but I am looking forward to enable browser
+  debugging.`
 
 function Greet (msg) {
   console.log(msg)
@@ -9,20 +11,22 @@ function Greet (msg) {
 
 Greet(title)
 
-// Hello
-var variables = [
-  'Hello World',
-  {
-    title: 'Some Object'
-  },
-  [1, 2, 3],
-  new Date(),
-  true,
-  123123,
-  function () {
-    console.log('hello')
-  }]
+let object = {
+  title: 'Some Object'
+}
 
-variables.forEach((m) => {
-  Greet(m)
-})
+Greet(object)
+// // Hello
+// var variables = [
+//   'Hello World',
+//   {
+//     title: 'Some Object'
+//   },
+//   [1, 2, 3],
+//   new Date(),
+//   true,
+//   123123]
+//
+// variables.forEach((m) => {
+//   Greet(m)
+// })
