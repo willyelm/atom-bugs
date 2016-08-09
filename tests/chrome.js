fe974ea7afcbb0671838fb7383bc565e036b598d
Williams Medina <williams.medinaa@gmail.com>
2016-08-09 07:03:07 -0400

    object inspector and fixes

--- tests/chrome.js
+++ tests/chrome.js
@@ -3,16 +3,27 @@ function Greet (msg) {
   console.log(msg)
 }
 // Hello
-var variables = [
-  'Hello World',
-  {
-    title: 'Some Object'
+let test = new Greet()
+let testString = 'Hello World Hello'
+let textObject = {
+  title: 'Some Object',
+  n: 123,
+  otherTest: {
+    value: 1
   },
-  [1, 2, 3],
-  new Date(),
-  true,
-  123123]
+  test () {
+    console.log('test')
+  }
+}
+let testArray = ['1', '2', '3']
+let testDate = new Date()
+let testBoolean = true
+let testNumber = 123123
 
-variables.forEach((m) => {
-  Greet(m)
-})
+Greet(test)
+Greet(testString)
+Greet(textObject)
+Greet(testArray)
+Greet(testDate)
+Greet(testBoolean)
+Greet(testNumber)
--- tests/node.js
+++ tests/node.js
@@ -1,21 +1,58 @@
 // Atom Bugs: Test
+function HelloWorld () {
+  this.message = 'bye'
+}
+class Another {
+  constructor () {
+    this.hello = 'my'
+  }
+}
+class MyClass extends Another {
+  constructor () {
+    super()
+    this.test = `${this.hello} test`
+    this.obj = {
+      othervalue: '123',
+      test () {
+        console.log('test')
+      },
+      subObject: {
+        value: '123'
+      },
+      subObject2: {
+        value2: '123'
+      }
+    }
+  }
+}
+let test = Object.create(HelloWorld)
+let test2 = new HelloWorld()
+let ref = test2
+let object = new Another()
+let object2 = new MyClass()
 var project = require('./node2')
-var variables = [
-  'Hello World',
-  {
-    title: 'Some Object'
-  },
-  [1, 2, 3],
-  new Date(),
-  true,
-  123123]
+let testString = 'Hello World Hello'
+let textObject = {
+  title: 'Some Object'
+}
+let testArray = ['1', '2', '3']
+let testDate = new Date()
+let testBoolean = true
+let testNumber = 123123
 
 function Greet (msg) {
   console.log(msg)
 }
 
-variables.forEach((m) => {
-  Greet(m)
-})
-
+Greet(test)
+Greet(test2)
+Greet(ref)
+Greet(object)
+Greet(object2)
+Greet(testString)
+Greet(textObject)
+Greet(testArray)
+Greet(testDate)
+Greet(testBoolean)
+Greet(testNumber)
 Greet(project.getMessage())
